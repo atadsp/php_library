c088ecce45897cdf072a6916b2e3037e85c3f84f
Chris Beckman <atadsp@gmail.com>
2016-09-12 16:54:16 -0400

    variables and stuff

---
+++ variables/index.php
@@ -0,0 +1,32 @@
+<?php 
+
+//Basic Variables
+$x = 55;
+$y = 983242;
+$z = $x + $y;
+
+//works the same way as in js. just REMEBER YOUR SEMICOLON
+$stuff = 'What would YOU do?';
+$things = 'For a Klondike Bar?';
+//this is one way to concatinate strings
+$stuff_things = "{$stuff} {$things}";
+//this is the other better way
+$things_stuff = $stuff . " " . $things;
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<!-- all inbody php must be written with <?php ?> -->
+<p> <?php echo $z ?> </p>
+<p> <?php echo $stuff_things; ?> </p>
+<!-- Print is another way to push things to the DOM -->
+<?php print("iohwojgoiwejgoiwejgiweo 3883893985897357893859"); ?>
+</body>
+</html>
---
+++ variables/variables.php
@@ -0,0 +1,9 @@
+<?php 
+
+//Basic Variables
+$x = 55;
+$y = 983242;
+$z = $x + $y
+echo $z
+
+?>
